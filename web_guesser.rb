d957be44334e6fa7f4c7c6ce1e85fc254c2d496b
Thomas Soucie <tsoucie1858@gmail.com>
2017-04-26 15:00:46 -0600

    Commits functioning messages

--- web_guesser.rb
+++ web_guesser.rb
@@ -7,11 +7,28 @@ class WebGuesser
       @secret_number = rand(100)
     end
 
+    def guess_checker(guess)
+      if guess > secret_number
+        message = "Too High!"
+      elsif guess < secret_number
+        message = "Too Low"
+      elsif guess == secret_number
+        message = "That's Just Right, you WIN!"
+      end
+    end
+
 end
 
-number = WebGuesser.new.secret_number
+guesser = WebGuesser.new
 
 get '/' do
-  # "The SECRET NUMBER IS #{number}"
-  erb :index, :locals => {:number => number}
+  guess_number = params['guess'].to_i
+  number = guesser.secret_number
+  message = guesser.guess_checker(guess_number)
+
+  erb :index, :locals => {
+    :number => number,
+    :guess => 'guess',
+    :message => message
+  }
 end
