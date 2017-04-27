9819fc34aed77e6c63b9a8d60c0dc7b0c8434441
Thomas Soucie <tsoucie1858@gmail.com>
2017-04-27 10:13:36 -0600

    Commits color extension to complete i4 and tutorial. If I have time over the weekend I want to refactor to clean up and/or add extensions but in the essence of time moving on to next intermission assignment.

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,20 +3,27 @@ require 'sinatra/reloader'
 
 class WebGuesser
   attr_reader :secret_number
+  attr_accessor :background
     def initialize
       @secret_number = rand(100)
+      @background = "FD1616"
     end
 
     def guess_checker(guess)
       if guess > (secret_number + 4)
+        @background = "#FD1616"
         message = "WAY to high"
       elsif guess < (secret_number - 4)
+        @background = "#FD1616"
         message = "WAY too low"
       elsif guess > secret_number
+        @background = "#F37171"
         message = "Too High!"
       elsif guess < secret_number
+        @background = "#F37171"
         message = "Too Low"
       elsif guess == secret_number
+        @background = "#35D83B"
         message = "You got it right, the secret number is #{secret_number}!"
       end
     end
@@ -32,6 +39,7 @@ get '/' do
 
   erb :index, :locals => {
     :number => number,
-    :message => message
+    :message => message,
+    :background_color => guesser.background
   }
 end
